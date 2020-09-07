8328ae7bdd835c4aec0ddd5bbe3dea0b5d66894a
Termux-Hackers <69192879+Termux-Hackers@users.noreply.github.com>
2020-09-07 09:03:05 +0500

    Add files via upload

---
+++ mbp/opt.sh
@@ -0,0 +1,25 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+cd $HOME/META-KIT/mbp
+echo " "
+echo " "
+echo -e "$ylo      To terminate the process click 'n' or to continue
+                   double click on ENTER$rset"
+read choice
+if [[ $choice = 'n' ]] ; then
+echo -e "$red    process is terminating? press ENTER to Terminate$rset"
+read choice
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+read choice
+cd $HOME/META-KIT/min
+bash metain.sh
+echo " "
+echo " "
+fi
