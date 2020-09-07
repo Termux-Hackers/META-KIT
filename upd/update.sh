0f296f69b68f7e6bf304909c0ee5c854a29a9346
Termux-Hackers <69192879+Termux-Hackers@users.noreply.github.com>
2020-09-07 09:33:11 +0500

    Update update.sh

--- upd/update.sh
+++ upd/update.sh
@@ -1,8 +1,50 @@
-#reboot bash
+
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
 echo " "
+echo " "
+echo '
+
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the META-KIT please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf META-KIT
+git clone https://github.com/noob-hackers/META-KIT
+clear
+echo " "
+echo -e "$grn               META-KIT has been updated check it now...$rset"
+sleep 3.0
 echo " "
 cd $HOME
 cd META-KIT
 bash META-KIT.sh
-echo " "
-echo " "
+clear
+sleep 1.0
