8b25780226d14dd1f02391fdece10f03b95c1cbd
Termux-Hackers <69192879+Termux-Hackers@users.noreply.github.com>
2020-09-07 09:34:18 +0500

    Add files via upload

---
+++ m-wiz.sh
@@ -0,0 +1,333 @@
+#SCRIPT BY SHOAIB DONT TRY TO COPY OR MODIFY 
+#META TOOL FOR ALL METASPLOIT THINGS
+#data/data/com.termux/files/usr/bin/bash
+#colours
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#coding section starts :)
+clear
+echo " "
+echo " "
+echo -e "$blue
+                         ╔═╗╔═╗╔═══╗╔════╗╔═══╗
+                         ║║╚╝║║║╔══╝║╔╗╔╗║║╔═╗║
+                         ║╔╗╔╗║║╚══╗╚╝║║╚╝║║─║║
+                         ║║║║║║║╔══╝──║║──║╚═╝║
+                         ║║║║║║║╚══╗──║║──║╔═╗║
+                         ╚╝╚╝╚╝╚═══╝──╚╝──╚╝─╚╝
+                                                                v 1.0$rset"
+echo " "                                                          
+echo -e "$red                                   ▶ Coded by$grn Shoaib$grn ◀$rset"
+echo -e "$red                              ⫸$ylo youtube.com/noobhackers$red ⫷$rset"
+echo " "
+echo " "
+echo -e "$pink                         >>>  [ click ENTER to continue ] <<<$rset"
+read nitrom
+echo " "
+echo " "
+echo -e "$cyan                   》META-KIT is a official tool made for metasploit《$rset"
+echo " "
+echo -e "$red                           [NOTE:- NEED INTERNET CONNECTION]$rset"
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [TOOLS]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$blue                             ➡$cyan [1️⃣] Metasploit install"
+echo -e "$blue                             ➡$cyan [2️⃣] Metasploit repair"
+echo -e "$blue                             ➡️$cyan [3️⃣] Metasploit backup"
+echo -e "$blue                             ➡$cyan [4️⃣] Metasploit restore"
+echo -e "$blue                             ➡️$cyan [5️⃣] Metasploit delete"
+echo -e "$blue                             ➡$cyan [6️⃣] Delete script"
+echo -e "$blue                             ➡️$cyan [7️⃣] Update script"
+echo -e "$blue                             ➡$cyan [8️⃣] About" 
+echo -e "$blue                             ➡️$cyan [9️⃣] Reboot"
+echo -e "$red                              ➡$cyan [🔟] Exit"
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                              [[[$ylo Select any option$cyan]]]$rset"
+echo " "
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo " "
+echo -e "$red                         You are going to$grn Install$red Metasploit$rset"
+echo " "
+echo -e "$grn                               Press$ylo ENTER$red to continue$rset"
+echo " "
+read hulke
+cd $HOME/META-KIT/min
+echo " "
+echo -e "$ylo         To terminate the process click$red 't'$ylo or to continue click on$grn ENTER$rset" 
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                           Are you sure? Press$grn ENTER$red to exit$rset"
+read hulkee
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash metain.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                       click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+
+2)echo " "
+echo " "
+echo " "
+echo -e "$red                          You are going to$grn Repair$red Metasploit$rset "
+echo " "
+echo -e "$grn                                Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitro
+cd $HOME/META-KIT/mrp
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                          Are you sure? Press$grn ENTER$red to exit$rset"
+read nitroo
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash metarpir.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                      click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+3)echo " "
+echo " "
+echo " "
+echo -e "$red                         You are going to$grn Back up$red Metasploit$rset "
+echo " "
+echo -e "$grn                                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+read mon
+cd $HOME/META-KIT/mbp
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                         Are you sure? Press$grn ENTER$red to exit$rset"
+read monn
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash backup.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+4)echo " "
+echo " "
+echo " "
+echo -e "$red                         You are going to$grn Restore$red Metasploit$rset "
+echo " "
+echo -e "$grn                                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+read gzil
+cd $HOME/META-KIT/mrs
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                          Are you sure? Press$grn ENTER$red to exit$rset"
+read gzill
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash restore.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+5)echo " "
+echo " "
+echo " "
+echo -e "$red                         You are going to$grn Delete$red Metasploit$rset "
+echo " "
+echo -e "$grn                                  Press$ylo ENTER$grn to continue$rset"
+echo " "
+read kong
+cd $HOME/META-KIT/mrp
+echo " "
+echo -e "$ylo          To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                        Are you sure? Press$grn ENTER$red to exit$rset"
+read kongg
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash olddelete.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+6)echo " "
+echo " "
+echo " "
+echo -e "$red                          You are going to$grn Delete$red script$rset"
+echo " "
+echo -e "$grn                                   Press$ylo ENTER$grn to continue$rset"
+echo " "
+read gidor
+cd $HOME/META-KIT/del
+echo " "
+echo -e "$ylo            To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                        Are you sure? Press$grn ENTER$red to exit$rset"
+read gidora
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash delete.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+7)echo " "
+echo " "
+echo " "
+echo -e "$red                          You are going to$grn Update$red script$rset"
+echo " "
+echo -e "$grn                                   Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nit
+cd $HOME/META-KIT/upd
+echo " "
+echo -e "$ylo            To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                       Are you sure? Press$grn ENTER$red to exit$rset"
+read nitt
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash update.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+8)echo " "
+echo " "
+echo " "
+echo -e "$red                           You are going to$grn open About$red Metasploit$rset "
+echo " "
+echo -e "$grn                                   Press$ylo ENTER$grn to continue$rset"
+echo " "
+read sim
+cd $HOME/META-KIT/abt
+echo " "
+echo -e "$ylo            To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                      Are you sure? Press$grn ENTER$red to exit$rset"
+read simm
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash about.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+9)echo " "
+echo " "
+echo " "
+echo -e "$red                          You are going to$grn Reboot$red Script$rset "
+echo " "
+echo -e "$grn                               Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitroz
+cd $HOME/META-KIT/rbt
+echo " "
+echo -e "$ylo            To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                      Are you sure? Press$grn ENTER$red to exit$rset"
+read nitrooz
+cd $HOME/META-KIT
+bash META-KIT.sh
+else
+bash reboot.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan                        click$grn ENTER$cyan to go back into META-KIT menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/META-KIT
+bash META-KIT.sh ;;
+10)
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+                       ____  _  _  ____    ____  _  _  ____ 
+                      (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+                      ) _ < \  /  )__)    ) _ < \  /  )__) 
+                      (____/ (__) (____)  (____/ (__) (____)...$cyan have a awesome day$rset"
+echo " "
+echo " "
+exit
+esac
